7d1b989b8780b4d53588fd0198a34b379a2d5794
user <stratecide@gmail.com>
2023-04-08 21:34:16 +0200

    camera follows player

--- src/game/camera/mod.rs
+++ src/game/camera/mod.rs
@@ -4,12 +4,15 @@ use bevy::prelude::*;
 use system::*;
 use crate::resource::Screen;
 
+use super::physics::PhysicsSystemSet;
+
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.
-            add_system(startup.in_schedule(OnEnter(Screen::Level)))
+        app
+            .add_system(startup.in_schedule(OnEnter(Screen::Level)))
+            .add_system(follow_player.in_set(PhysicsSystemSet::AfterMovement))
             ;
     }
 }
--- src/game/camera/system.rs
+++ src/game/camera/system.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 use crate::game::medicine::resource::SideEffects;
+use crate::game::player::component::Player;
 use crate::resource::LevelHandle;
 use crate::level_loader::Level;
 
@@ -19,5 +20,20 @@ pub fn startup(
     camera.transform.scale.x = 0.5;
     camera.transform.scale.y = 0.5;
     commands.spawn(camera);
+}
+
+pub fn follow_player(
+    player_query: Query<(&Transform, &Player), Without<Camera>>,
+    mut camera_query: Query<&mut Transform, With<Camera>>,
+    level_handle: Res<LevelHandle>,
+    level_assets: Res<Assets<Level>>,
+    side_effects: Res<SideEffects>,
+) {
+    if let Ok((player_transform, _)) = player_query.get_single() {
+        let level_data = level_assets.get(&level_handle.handle).unwrap();
+
+        let mut camera_transform = camera_query.get_single_mut().unwrap();
+        camera_transform.translation = player_transform.translation;
+    }
 }
 
